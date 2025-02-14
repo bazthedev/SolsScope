76b16923536a0828eb047d242d105d162992f75f
Baz <bazthedev@gmail.com>
2025-02-14 16:23:58 +0000

    Update main.py
    + Aura Detection
    + Plugin Support
    + Automatically Download Latest Aura List
    + Tweaked some settings

--- main.py
+++ main.py
@@ -8,7 +8,16 @@ from pynput import mouse, keyboard
 from pynput.mouse import Button
 from pynput.keyboard import Key
 import asyncio
+from PIL import ImageGrab
+import requests
 
+def get_auras():
+    print("Downloading Aura List")
+    dl = requests.get("https://github.com/bazthedev/SolsRNGBot/raw/refs/heads/main/auras.json")
+    f = open("auras.json", "wb")
+    f.write(dl.content)
+    f.close()
+    print("Downloaded Aura List")
 
 def update_settings(settings, timer : int, ssnorm : bool, ssstor : bool, ssinv : bool, log_channel_id : int):
     settings["timer"] = timer
@@ -31,9 +40,12 @@ async def reset_mouse_pos():
 if not os.path.exists("./scr/"):
     os.mkdir("./scr/")
 
+if not os.path.exists("./plugins/"):
+    os.mkdir("./plugins/")
+
 if not os.path.exists("./settings.json"):
     x = open("settings.json", "w")
-    x.write('{"timer" : 1800, "ssnorm" : true, "ssstor" : true, "ssinv" : true, "log_channel_id": 0}')
+    x.write('{"TOKEN": "", "__version__" : "1.0.1", "timer" : 1800, "ssnorm" : true, "ssstor" : true, "ssinv" : true, "log_channel_id": 0}')
     x.close()
 
 now = datetime.now()
@@ -46,6 +58,11 @@ default_pos = (1280, 720) # change this to the position of your mouse after shif
 resting_pos = (-942, 604) # change this to a position outside of the game window
 close_pos = (1887, 399) # change this to the position of the X after you open aura or inventory menu
 # These values can be obtained by using the get_mouse_pos.py script, and moving your mouse over the buttons.
+_plugins = []
+
+
+def rgb2hex(r,g,b):
+    return "#{:02x}{:02x}{:02x}".format(r,g,b)
 
 def reload_settings():
     global settings, wait_time, scr_norm, scr_stor, scr_inv, log_channel_id
@@ -59,11 +76,17 @@ def reload_settings():
     print(settings)
 
 reload_settings()
+get_auras()
 
+with open("auras.json", "r") as f:
+    auras = json.load(f)
+
+__version__ = settings["__version__"]
 @client.event
 async def on_ready():
     print("Let's go gambling!")
-    print(f"Started at {now.strftime("%d/%m/%Y %H:%M:%S")}")
+    print(f"Started at {now.strftime("%d/%m/%Y %H:%M:%S")} running version {__version__}")
+    await client.change_presence(activity=discord.Game(name=f"bazthedev/SolsRNGBot version {__version__}"))
     if log_channel_id != 0:
         log_channel = client.get_channel(log_channel_id)
         emb = discord.Embed(
@@ -71,15 +94,19 @@ async def on_ready():
             description=f"Started at {now.strftime("%d/%m/%Y %H:%M:%S")}"
         )
         await log_channel.send(embed=emb)
-        get_screenshots.start()
-        print("Started screenshots")
+        if settings["ssnorm"] or settings["ssstor"] or settings["ssinv"]:
+            get_screenshots.start()
+            print("Started screenshots")
+        aura_detection.start()
+        print("Started Aura Detection")
     else:
-        print("You must select a channel ID, you can do this by running the set_log_channel command.") # It is recommended that you restart the bot after using this command, however it will still send messages to the new specified channel
+        print("You must select a channel ID, you can do this by running the set_log_channel command.")
     await asyncio.sleep(60)
     keep_alive.start()
     print("Started keep alive")
 
 @client.command()
+@commands.is_owner()
 async def update_timer(ctx, new_time : int):
     global wait_time, scr_norm, scr_inv, scr_stor, log_channel_id
     if new_time != wait_time:
@@ -90,6 +117,7 @@ async def update_timer(ctx, new_time : int):
         await ctx.send("Wait duration has not been updated.")
 
 @client.command()
+@commands.is_owner()
 async def set_log_channel(ctx):
     await ctx.send("Updating log channel...")
     global wait_time, scr_norm, scr_inv, scr_stor, log_channel_id
@@ -99,6 +127,7 @@ async def set_log_channel(ctx):
     await ctx.send(f"Log Channel set to {ctx.message.channel.mention}")
 
 @client.command()
+@commands.is_owner()
 async def stop(ctx):
     await ctx.send("Manual stop initiated")
     get_screenshots.stop()
@@ -107,6 +136,7 @@ async def stop(ctx):
     print("Aw dang it")
 
 @client.command()
+@commands.is_owner()
 async def manual_scr(ctx):
     await ctx.send("Taking screenshots, please wait, this will take about 30 seconds.")
     if settings["ssnorm"]:
@@ -168,6 +198,7 @@ async def get_screenshots():
         print("You must select a channel ID, you can do this by running the set_log_channel command.")
         return
     log_channel = client.get_channel(log_channel_id)
+    await log_channel.send("Taking screenshots, please wait, this will take about 30 seconds")
     if settings["ssnorm"]:
         _mouse.position = resting_pos
         await asyncio.sleep(1)
@@ -237,5 +268,78 @@ async def keep_alive():
     await asyncio.sleep(1)
     _keyboard.release(Key.space)
 
+@client.command()
+async def shutdown(ctx):
+    await ctx.send("Shutting down in 1 minute")
+    os.system("shutdown /s /t 60")
 
-client.run("TOKEN") # replace this with your discord bot token
+@client.command()
+async def s(ctx):
+    await ctx.send("Shutting down now")
+    os.system("shutdown /s /t 0")
+
+@client.command()
+async def cancel(ctx):
+    await ctx.send("Cancelling shutdown")
+    os.system("shutdown /a")
+
+@client.command()
+@commands.is_owner()
+async def scr(ctx):
+    if os.path.exists("./scr/screenshot.png"):
+        os.remove("./scr/screenshot.png")
+        await asyncio.sleep(1)
+    img = pag.screenshot("./scr/screenshot.png", allScreens=True)
+    await ctx.send(file=discord.File("./scr/screenshot.png"))
+
+@tasks.loop(seconds=0)
+async def aura_detection():
+    if log_channel_id == 0:
+        print("You must select a channel ID, you can do this by running the set_log_channel command.")
+        return    
+    px = ImageGrab.grab().load()
+    colour = px[default_pos[0], default_pos[1]]
+    hex_col = rgb2hex(colour[0], colour[1], colour[2])
+    try:
+        check = auras[hex_col]
+        aura_detection.stop()
+    except KeyError:
+        pass
+
+@client.command()
+async def plugins(ctx):
+    await ctx.send("Installed Plugins:")
+    for plu in _plugins:
+        await ctx.send(plu)
+    
+
+@aura_detection.after_loop
+async def on_aura_detection_cancel():
+        rnow = datetime.now()
+        px = ImageGrab.grab().load()
+        colour = px[default_pos[0], default_pos[1]]
+        hex_col = rgb2hex(colour[0], colour[1], colour[2])
+        if os.path.exists("./scr/screenshot_aura.png"):
+            os.remove("./scr/screenshot_aura.png")
+            await asyncio.sleep(1)
+        auraimg = pag.screenshot("./scr/screenshot_aura.png")
+        up = discord.File("./scr/screenshot_aura.png", filename="aura.png")
+        emb = discord.Embed(
+            title = f"Aura Rolled: {auras[hex_col]["name"]}",
+            description = f"Rolled Aura: {auras[hex_col]["name"]}\nWith chances of 1/{auras[hex_col]["rarity"]}\nAt time: {rnow.strftime("%d/%m/%Y %H:%M:%S")}\nDetected Colour: {hex_col}",
+            colour = discord.Color.from_rgb(colour[0], colour[1], colour[2])
+        )
+        emb.set_image(url="attachment://aura.png")
+        log_channel = client.get_channel(log_channel_id)
+        await log_channel.send(embed=emb, file=up)
+        await asyncio.sleep(10)
+        aura_detection.restart()
+
+
+for filename in os.listdir("./plugins"):
+    if filename.endswith(".py"):
+        client.load_extension(f"plugins.{filename[:-3]}")
+        print(f"Loaded plugin: {filename[:-3]}")
+        _plugins.append(filename[:-3])
+
+client.run(settings["TOKEN"])

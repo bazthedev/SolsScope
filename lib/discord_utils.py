b94de8502fc40f25354aca530ae4553058814707
Baz <88540049+bazthedev@users.noreply.github.com>
2025-04-25 17:15:47 +0100

    Update discord_utils.py

--- lib/discord_utils.py
+++ lib/discord_utils.py
@@ -1,3 +1,10 @@
+"""
+SolsScope/Baz's Macro
+Created by Baz and Cresqnt
+v1.2.5
+Support server: https://discord.gg/6cuCu6ymkX
+"""
+
 import sys
 import os
 sys.path.insert(1, os.path.expandvars(r"%localappdata%/SolsScope/lib"))
@@ -47,7 +54,7 @@ def forward_webhook_msg(primary_webhook_url: str, secondary_urls: list, *, file=
             continue
 
         try:
-            _webhook = discord.Webhook.from_url(webhook_url, adapter=discord.RequestsWebhookAdapter())
+            _webhook = discord.SyncWebhook.from_url(webhook_url)
 
             current_kwargs = kwargs.copy()
             if image_bytes and filename:

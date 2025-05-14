aac28151b11c153dab3bac83b2a887145c9846d8
Baz <88540049+bazthedev@users.noreply.github.com>
2025-05-14 15:59:07 +0100

    Update get_mouse_pos.py

--- test_scripts/get_mouse_pos.py
+++ test_scripts/get_mouse_pos.py
@@ -1,7 +1,15 @@
 from pynput import mouse
 from pynput.mouse import Button
+from mousekey import MouseKey
 
+mkey = MouseKey()
+
+mkey.enable_failsafekill("ctrl+e")
 _mouse = mouse.Controller()
 
+previous_pos = None
+
 while True:
-    print(f"{str(_mouse.position)}")
+    if previous_pos != _mouse.position:
+        print(f"({str(_mouse.position[0])}/2560, {str(_mouse.position[1])}/1440)")
+        previous_pos = _mouse.position
